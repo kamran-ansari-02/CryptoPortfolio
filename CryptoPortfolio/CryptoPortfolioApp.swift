abaec57a13356beaf5fd0f9b7c90e25a886cbace
kamran-ansari-02 <150513230+kamran-ansari-02@users.noreply.github.com>
2025-08-25 20:20:51 +0530

    Initial Commit

---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by kamran ansari on 25/08/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoPortfolioTests/CryptoPortfolioTests.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioTests.swift
+//  CryptoPortfolioTests
+//
+//  Created by kamran ansari on 25/08/25.
+//
+
+import Testing
+@testable import CryptoPortfolio
+
+struct CryptoPortfolioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
